646fe648b31ea54294d8be74be3d3b97dc9e4c0b
Sergei <saboteurincave@gmail.com>
2017-01-09 14:22:52 +0300

    add checking missed permissions

--- CSchoolSite/applications/apps.py
+++ CSchoolSite/applications/apps.py
@@ -1,26 +1,21 @@
 from django.apps import AppConfig
-
 from django.utils.translation import ugettext_lazy as _
 
 
-def create_permissions(permissions_list, model_name):
-    from django.contrib.auth.models import Permission
-    return [Permission.objects.get(codename="{}_{}".format(permission, model_name)) for permission in permissions_list]
-
-
 def create_groups(sender, **kwargs):
-    import logging
     from django.contrib.auth.models import Group, Permission
 
+    permissions_list = ['add', 'change', 'delete', 'view']
+    model_name = 'campvoucher'
     group, created = Group.objects.get_or_create(name=_('Комитет образования'))
 
-    if created:
-        permissions = create_permissions(['add', 'change', 'delete', 'view'], 'campvoucher')
-        for permission in permissions:
-            if permission not in group.permissions:
+    for permission_name in permissions_list:
+        try:
+            permission = Permission.objects.get(codename="{}_{}".format(permission_name, model_name))
+            if permission not in group.permissions.all():
                 group.permissions.add(permission)
-    else:
-        logging.error('Cannot create group for committee!')
+        except Exception:
+            pass
 
 
 class ApplicationsConfig(AppConfig):
