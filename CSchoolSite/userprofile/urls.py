2ee5fe0fbe26c33e49a977b9012a563108f79322
Tsarev Nikita <nikts53@gmail.com>
2017-01-18 18:17:41 +0300

    Password change / reset stuff

--- CSchoolSite/userprofile/urls.py
+++ CSchoolSite/userprofile/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from registration.backends.simple.views import RegistrationView
 from userprofile.forms import ExtendedRegistrationForm
 from userprofile import views
-
-from CSchoolSite import settings
+from django.contrib.auth import views as auth_views
 
 
 class ExtendedRegistrationView(RegistrationView):
@@ -12,6 +10,14 @@ class ExtendedRegistrationView(RegistrationView):
 
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^password_change/$', auth_views.password_change, name='password_change'),
+    url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^logout/$', views.logout, name='auth_logout'),
     url(r'^register/$', ExtendedRegistrationView.as_view(), name='registration_register'),
     url(r'', include('registration.backends.simple.urls')),
