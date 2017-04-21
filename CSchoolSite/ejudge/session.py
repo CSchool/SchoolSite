1352ec0f84cc7a0588b87844db303e803f51f1ce
TsarN <nikts53@gmail.com>
2017-04-21 16:51:08 +0300

    ejudge sessions now can use libejudge to fetch sessions

--- CSchoolSite/ejudge/session.py
+++ CSchoolSite/ejudge/session.py
@@ -8,6 +8,35 @@ cached_session_id = None
 cached_session_time = None
 
 
+def do_subprocess():
+    global cached_session_time, cached_session_id
+    try:
+        cached_session_id = subprocess.check_output((
+            EJUDGE_CONTESTS_CMD_PATH,
+            str(EJUDGE_CONTEST_ID),
+            "master-login",
+            "STDOUT",
+            EJUDGE_USER_LOGIN,
+            EJUDGE_USER_PASSWORD
+        ))
+        cached_session_id = cached_session_id.decode().strip()
+    except:
+        return False
+    return True
+
+
+def do_libejudge():
+    global cached_session_time, cached_session_id
+    try:
+        import ejudge.libejudge as libejudge
+        clnt = libejudge.Userlist()
+        r = clnt.privLogin('127.0.0.1', 0, EJUDGE_CONTEST_ID, 0, 6, EJUDGE_USER_LOGIN, EJUDGE_USER_PASSWORD)
+        cached_session_id = "%016x-%016x" % (r['sid'], r['client_key'])
+    except:
+        return False
+    return True
+
+
 def get_session():
     """
     Get ejudge session string
@@ -15,17 +44,7 @@ def get_session():
     """
     global cached_session_time, cached_session_id
     if cached_session_id is None or (datetime.datetime.now() - cached_session_time).seconds > EJUDGE_SESSION_TIMEOUT:
-        try:
-            cached_session_id = subprocess.check_output((
-                EJUDGE_CONTESTS_CMD_PATH,
-                str(EJUDGE_CONTEST_ID),
-                "master-login",
-                "STDOUT",
-                EJUDGE_USER_LOGIN,
-                EJUDGE_USER_PASSWORD
-            ))
-        except subprocess.CalledProcessError:
-            return None
-        cached_session_id = cached_session_id.decode().strip()
+        if not do_libejudge():
+            do_subprocess()
         cached_session_time = datetime.datetime.now()
     return cached_session_id
