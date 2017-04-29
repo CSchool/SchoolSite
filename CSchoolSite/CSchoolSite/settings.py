2da2e5c248b1b3131eaed73d422c84c49b187013
TsarN <nikts53@gmail.com>
2017-04-29 13:20:24 +0300

    Removed most of password validators - who needs them anyway?

--- CSchoolSite/CSchoolSite/settings.py
+++ CSchoolSite/CSchoolSite/settings.py
@@ -123,17 +123,12 @@ NOTIFICATIONS_USE_JSONFIELD=True
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        'OPTIONS':
+        {
+            'min_length': 5,
+        }
+    }
 ]
 
 AUTH_USER_MODEL = 'userprofile.User'
